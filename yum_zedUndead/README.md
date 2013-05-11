6e34951ccedb3587a12e8d436da382893b24d041
mmmyum <matt@mattsaunders.info>
2013-05-11 16:22:58 -0400

    Initial Commit
    Untested code WIP

---
+++ yum_zedUndead/README.md
@@ -0,0 +1,14 @@
+README
+
+pre pre pre pre release
+
+This Git will be the home for my dayz projects, going to migrate from pastebin.
+
+
+Specifically for my dayz zed changes, there will be two sections - mission and addon
+
+Mission = new spawning script, permanent loot and zeds, rewritten ai, speed very slow or normal fast or faster
+
+Addon = new zed animations, taken from Undead Mod, allows completely customizable speeds (medium pace!)
+
+everything coming soon, do not use code at this time.
---
+++ yum_zedUndead/config.cpp
@@ -0,0 +1,18 @@
+class undeadYum
+ {
+ units[]={};
+ weapons[]={};
+ requiredAddons[]= {dayz_anim,dayz_code,dayz_weapons,dayz_equip,dayz_vehicles};
+};
+
+
+class CfgMovesZombie : CfgMovesBasic {
+        skeletonName = "OFP2_ManSkeleton";
+        gestures = "CfgGesturesMale";
+
+        class AmovPercMwlkSnonWnonDf { //removed : AmovPercMstpSnonWnonDnon
+                                file = "\yum_zedUndead\zmb\zombie3_walk"; // new animation file
+                                speed = 0.6; //originally 0.2 in dayz
+                                //duty = -0.7;
+                        };
+};
