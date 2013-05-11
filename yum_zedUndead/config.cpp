7ede0fd66cb315f0870e4b6be91ac27fbe5bcca6
mmmyum <matt@mattsaunders.info>
2013-05-11 18:34:00 -0400

    more pack attempts
    packs fine without errors on binarize - but log file shows some issues

--- yum_zedUndead/config.cpp
+++ yum_zedUndead/config.cpp
@@ -5,8 +5,8 @@ class CfgPatches   {
       requiredAddons[]= {dayz_anim,dayz_code,dayz_weapons,dayz_equip,dayz_vehicles};
    };
 };
-
 class CfgMovesZombie {
+
         class AmovPercMwlkSnonWnonDf {
                                 file = "\yum_zedUndead\zmb\zombie3_walk"; 
                                 speed = 0.6; //originally 0.2 in dayz
